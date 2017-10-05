55ed525159492649a6c88c697603e079003ef65b
U-C\mscuffham <mscuffham@LT-CNU350BHW7.c.systemsinmotion.com>
2017-10-05 13:57:49 -0400

    Added form for creating new timesheets.

--- app/controllers/time_sheets_controller.rb
+++ app/controllers/time_sheets_controller.rb
@@ -6,4 +6,17 @@ class TimeSheetsController < ApplicationController
   def new
   	@time_sheet = TimeSheet.new
   end
+
+  def create
+  	@time_sheet = TimeSheet.new(time_sheet_params)
+  	@time_sheet.user = current_user
+  	@time_sheet.save
+  	redirect_to action: :index
+  end
+
+  private 
+  	def time_sheet_params
+  		params.require(:time_sheet).permit(:start_date)
+  	end
+
 end
